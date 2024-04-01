deadd8d059db4ca071ace126eb00e54fee5609e5
Rishi gupta <rishu.sic1608@gmail.com>
2024-04-01 16:07:54 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "site_blue": "#1c71ff !important",
+        "sub_text": "#616060 !important" 
+      },
+    },
+  },
+  plugins: [],
+};
